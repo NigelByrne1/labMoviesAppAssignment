238f0478cd1b70fafad3149ebc1ab32e030edd61
Nigel <nigelbyrnedesign@gmail.com>
2025-08-27 01:49:40 +0100

    Movie Details page UI upgrade (incomplete)

--- src/components/templateMoviePage/index.tsx
+++ src/components/templateMoviePage/index.tsx
@@ -43,16 +43,34 @@ const TemplateMoviePage: React.FC<TemplateMoviePageProps> = ({movie, children}) 
     }
 
     const images = data as MovieImage[];
+    const slicedImages = images.slice(0, 4); 
+    const mainImage = slicedImages[0];
+    
 
     return (
         <>
+
             <MovieHeader {...movie} />
 
+            
+            {mainImage ? (
+                <img
+                    src={`https://image.tmdb.org/t/p/w500/${mainImage.file_path}`}
+                    alt={movie.title}
+                    style={{
+                        width: "25%",
+                        maxHeight: "500px",
+                        objectFit: "contain",
+                        marginBottom: "16px",
+                        display: "block",
+                    }}
+                />
+            ) : null}
             <Grid container spacing={5} style={{ padding: "15px" }}>
                 <Grid item xs={3}>
                     <div>
                         <ImageList cols={1}>
-                            {images.map((image: MovieImage) => (
+                            {slicedImages.map((image: MovieImage) => (
                                 <ImageListItem
                                     key={image.file_path}
                                     sx={styles.gridListTile}
@@ -60,7 +78,7 @@ const TemplateMoviePage: React.FC<TemplateMoviePageProps> = ({movie, children}) 
                                 >
                                     <img
                                         src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                                        alt={'Image alternative'}
+                                        alt={movie.title}
                                     />
                                 </ImageListItem>
                             ))}
